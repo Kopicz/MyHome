ca39b57f6366085924b303861f3d16d4c9339537
Kopicz <geovanekopicz@gmail.com>
2015-12-11 16:32:14 -0200

    commit de mudancas

--- TestesSite/OK/intra/intra_OS.php
+++ TestesSite/OK/intra/intra_OS.php
@@ -50,6 +50,10 @@ header("Location: ../index1.php");
           width: 20%;
           float: left;
         }
+        #ConteudoConsult{
+          height: 300px;
+          color: red;
+        }
         .btn-primary{
           width: 200px;
 
@@ -144,10 +148,25 @@ header("Location: ../index1.php");
                             <a href="consultaClient.php?page=1"> <button  type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-sm" width="500px">
                             <span class="glyphicon glyphicon-user" style="font-size:48px" text-align="center";></span><br>CONSULTA CLIENTES</button></a>
 <!--/4 BOTAO-->         </div>
-                            <br>
+                           <p>
 <!--5 BOTAO-->          <div id="divBotoes">
                           <a href="consultaprod.php?page=1"> <button id="btnMenuIntra" type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-sm" width="500px">
                           <span class="glyphicon glyphicon-barcode" style="font-size:48px" text-align="center";></span><br>CONSULTA PRODUTOS</button></a>
+<!--/5 BOTAO-->         </div>
+                        <p>
+                        <div id="divBotoes">
+                          <a href="consultaprod.php?page=1"> <button id="btnMenuIntra" type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-sm" width="500px">
+                          <span class="glyphicon glyphicon-eye-open" style="font-size:48px" text-align="center";></span><br>CONSULTA FORNECEDOR</button></a>
+<!--/5 BOTAO-->         </div>
+                        <p>
+                        <div id="divBotoes">
+                          <a href="consultaprod.php?page=1"> <button id="btnMenuIntra" type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-sm" width="500px">
+                          <span class="glyphicon glyphicon-road" style="font-size:48px" text-align="center";></span><br>WEB O.S</button></a>
+<!--/5 BOTAO-->         </div>
+                        <p>
+                        <div id="divBotoes">
+                          <a href="consultaprod.php?page=1"> <button id="btnMenuIntra" type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-sm" width="500px">
+                          <span class="glyphicon glyphicon-fire" style="font-size:48px" text-align="center";></span><br>SUPORTE</button></a>
 <!--/5 BOTAO-->         </div>
 <!--/3 BOTAO-->       </div>    
 <!--/2 BOTAO-->     </div>    
@@ -155,111 +174,11 @@ header("Location: ../index1.php");
 <!--/1 BOTAO-->  </div>   
                   
 <!--consulta-->
-<!--1 conteudo--><div class="panel panel-primary" id="divConteudoPrincipal">
-                   <div class="panel-heading" ><center><b>Consulta Clientes<!--/2--></b></center></div>
-                    <?php include 'conexao.php' ?>
-                  <?php
-                  $page = $_GET["page"];
-                  if(isset($page)) {
-                  $page = $page;
-                  } else {
-                  $page = 1;
-                  }
-    //verifica a página atual caso seja informada na URL, senão atribui como 1ª página
-      //  $pagina = (isset($_GET['pagina']))? $_GET['pagina'] : 1;
- 
-    //seleciona todos os itens da tabela
-        $consulta = "SELECT * FROM clientes";
-        $clientes = mysql_query($consulta);
-   
-    //conta o total de itens
-        $total = mysql_num_rows($clientes);
-   
-    //seta a quantidade de itens por página, neste caso, 2 itens
-        $registros = 10;
-        $max_links = 3;
-    // Exibe o primeiro link "primeira página", que não entra na contagem acima(3)
-   
-    //calcula o número de páginas arredondando o resultado para cima
-        $numPaginas = ceil($total/$registros);
-   
-    //variavel para calcular o início da visualização com base na página atual
-        $inicio = ($registros*$page)-$registros;
- 
-    //seleciona os itens por página
-        
-        $consulta = "SELECT * FROM clientes order by CODIGO LIMIT $inicio , $registros";
-        $clientes = mysql_query($consulta);
-        $total = mysql_num_rows($clientes);
- 
-    echo '<table class="table table-hover">';  // opening table tag
-echo '<thead>
-      <tr>
-        <th class="info">Codigo</th>
-        <th class="info">CPF/CNPJ</th>
-        <th class="info">Razao</th>
-        <th class="info">Nome</th>
-        <th class="info">Endereço</th>
-        <th class="info">Numero</th>
-    </tr>
-    </thead>';
-while ($produto = mysql_fetch_array($clientes)) {
-echo '<tr>
+<!--1 conteudo--><div class="panel panel-primary" id="divConteudoPrincipal"  style="background-color:#FFF" id="ConteudoConsult">
+                   <div class="panel-heading" ><center><b>SEJAM BEM VINDOS!!!<!--/2--></b></center></div>
+                  
 
-      <td>'.$produto['CODIGO'].'</td>
-      <td>'.$produto['CPFCNPJ'].'</td>
-      <td>'.$produto['RAZAO'].'</td>
-      <td>'.$produto['NOME'].'</td>
-      <td>'.$produto['ENDERECO'].'</td>
-      <td>'.$produto['NUMERO'].'</td>';
-echo '</tr>'; 
-}
-echo '</table>';
-;
-         
-echo '
-<center>
-<nav>
-  <ul class="pagination">
-    <li>';
-      echo "<a href='consultaClient.php?page=1'>primeira pagina</a> ";
-      echo '</a>
-    </li>';
-
-
-// Cria um for() para exibir os 3 links antes da página atual
-for($i = $page-$max_links; $i <= $page-1; $i++) {
-// Se o número da página for menor ou igual a zero, não faz nada
-// (afinal, não existe página 0, -1, -2..)
-if($i <=0) {
-//faz nada
-// Se estiver tudo OK, cria o link para outra página
-} else {
-echo "<li><a href='consultaClient.php?page=$i'>$i</a></li>";
-}
-}
-// Exibe a página atual, sem link, apenas o número
-echo "<li class='disabled'><a href='consultaClient.php?page=$i'>$i</a></li>";
-// Cria outro for(), desta vez para exibir 3 links após a página atual
-for($i = $page+1; $i <= $page+$max_links; $i++) {
-// Verifica se a página atual é maior do que a última página. Se for, não faz nada.
-if($i > $numPaginas)
-{
-
-}
-// Se tiver tudo Ok gera os links.
-else
-{
-echo "<li><a href='consultaClient.php?page=$i'>$i</a></li>";
-}
-}
-// Exibe o link "última página"
-    echo '<li>';
-    echo "<a href='consultaClient.php?page=$numPaginas'>ultima pagina</a> ";
-    echo '</li>
-  </ul>
-</nav>';
-?>
+                   <center><img src="../img/hack.gif" height="410"></center>
 <!--/1 conteudo--></div>    
                   <!--final consulta-->
 
